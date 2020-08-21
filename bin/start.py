76e261eba1d17dd8a7131d77a997c19bba74d62c
yongxiaodong <1317665590@qq.com>
2020-08-21 14:37:36 +0800

    修改sys.path

--- bin/start.py
+++ bin/start.py
@@ -4,12 +4,13 @@
 
 import sys
 import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from core import src
 from lib import common_log
 
 logger = common_log.get_logger(__name__)
-
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 if __name__ == "__main__":
@@ -18,4 +19,3 @@ if __name__ == "__main__":
         src.run('MySQL_Sessions,MySQL_InnoDBDataReadWriten,MySQL_InnoDBBufferRatio,MySQL_QPSTPS')
     except Exception as e:
         logger.error(e)
-
